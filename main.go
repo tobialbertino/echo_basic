19a2f99ff2c26d4193f9ce709b09a34becb7f5d7
tobialbertino <tobialbertino@gmail.com>
2024-09-04 11:09:27 +0700

    fix: handle cors header

--- main.go
+++ main.go
@@ -12,6 +12,11 @@ func main() {
 	e.Use(
 		middleware.Logger(),
 		middleware.Recover(),
+		middleware.CORSWithConfig(middleware.CORSConfig{
+			AllowOrigins: []string{"http://127.0.0.1:5173", "http://localhost:5173"},
+			AllowMethods: []string{echo.GET, echo.POST, echo.PUT, echo.DELETE},
+			AllowHeaders: []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept},
+		}),
 	)
 
 	app.InitApp(e)
